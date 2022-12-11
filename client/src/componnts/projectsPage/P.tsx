88fb16c4022399525d0bacc758ffe0b4e4d8de9b
eli-Eliyahu <eli.eliyahu@280gmail.com>
2022-12-11 13:26:59 +0200

    b

--- client/src/componnts/projectsPage/P.tsx
+++ client/src/componnts/projectsPage/P.tsx
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import { useState } from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
@@ -18,7 +18,7 @@ import { useNavigate } from "react-router-dom";
 
 interface IProps {
   data: IDtat[] | undefined;
-  userName?:string
+  userName?: string;
   onId: (id: string) => void;
 }
 interface IDtat {
@@ -28,108 +28,125 @@ interface IDtat {
   situation: string;
 }
 
-const P = ({ data ,userName,onId}: IProps) => {
+const P = ({ data, userName, onId }: IProps) => {
   const navigte = useNavigate();
   return (
     <>
       {/* End hero unit */}
-<Typography variant="h3" sx={{mt:6}}>{userName ? `ברוך הבא  ${userName}` : "ברוך הבא"}</Typography>
-      <Grid
-        item
-        sx={{ mt: 8 }}
-        direction="row"
-        justifyContent="center"
-        alignItems="center"
+      <Typography variant="h3" sx={{ mt: 6 }}>
+        {userName ? `ברוך הבא  ${userName}` : "ברוך הבא"}
+      </Typography>
+      <Box
+        sx={{
+          mt: 8,
+          // display: "flex",
+        }}
       >
-        {data?.map((tier) => (
-          // Enterprise card is full width at sm breakpoint
-          <Grid
-            item
-            sx={{ mt: 3 }}
-            key={tier.name}
-            xs={12}
-            sm={8}
-            //   sm={tier.title === "Enterprise" ? 12 : 6}
-            md={4}
-          >
-            <Card>
-              <CardHeader
-                title={<Typography variant="h3" align="center"><StarIcon />{`${tier.name}`}</Typography>}
-                // subheader={tier.name}
-                titleTypographyProps={{ align: "center" }}
-                //   action={tier.title === "Pro" ? <StarIcon /> : null}
-                subheaderTypographyProps={{
-                  align: "center",
-                }}
-                sx={{
-                  backgroundColor: (theme) =>
-                    theme.palette.mode === "light"
-                      ? theme.palette.grey[200]
-                      : theme.palette.grey[700],
-                }}
-              />
-              <CardContent>
-                <Box
+        <Grid container>
+          {data?.map((tier) => (
+            // Enterprise card is full width at sm breakpoint
+
+            <Grid
+              item
+              sx={{
+                m: 3,
+              }}
+              key={tier.name}
+              xs={12}
+              sm={3}
+              //   sm={tier.title === "Enterprise" ? 12 : 6}
+              md={3}
+              lg={3}
+            >
+              <Card>
+                <CardHeader
+                  title={
+                    <Typography variant="h3" align="center">
+                      <StarIcon />
+                      {`${tier.name}`}
+                    </Typography>
+                  }
+                  // subheader={tier.name}
+                  titleTypographyProps={{ align: "center" }}
+                  //   action={tier.title === "Pro" ? <StarIcon /> : null}
+                  subheaderTypographyProps={{
+                    align: "center",
+                  }}
                   sx={{
-                    display: "flex",
+                    backgroundColor: (theme) =>
+                      theme.palette.mode === "light"
+                        ? theme.palette.grey[200]
+                        : theme.palette.grey[700],
+                  }}
+                />
+                <CardContent>
+                  <Box
+                    sx={{
+                      display: "flex",
+                      justifyContent: "center",
+                      alignItems: "baseline",
+                      mb: 2,
+                    }}
+                  >
+                    <Typography
+                      component="h2"
+                      variant="h5"
+                      color="text.primary"
+                    >
+                      {`סטטוס - ${tier.status}`}
+                    </Typography>
+                    <Typography
+                      variant="h5"
+                      color="text.primary"
+                      sx={{ mr: 5 }}
+                    >
+                      {`מצב - ${tier.situation}`}
+                    </Typography>
+                  </Box>
+                  <ul>
+                    {/* {tier.description.map((line) => ( */}
+                    {/* <Typography */}
+                    {/* component="li" */}
+                    {/* variant="subtitle1" */}
+                    {/* align="center" */}
+                    {/* key={line} */}
+                    {/* > */}
+                    {/* {line} */}
+                    {/* </Typography> */}
+                    {/* ))} */}
+                  </ul>
+                </CardContent>
+                <CardActions
+                  sx={{
+                    direction: "row",
                     justifyContent: "center",
-                    alignItems: "baseline",
-                    mb: 2,
-                   
-
+                    alignItems: "center",
                   }}
                 >
-                  <Typography component="h2" variant="h5" color="text.primary" >
-                  {`סטטוס - ${tier.status}`}
-                  </Typography>
-                  <Typography variant="h5" color="text.primary" sx={{mr:5}}>
-                    {`מצב - ${tier.situation}`}
-                  </Typography>
-                </Box>
-                <ul>
-                  {/* {tier.description.map((line) => ( */}
-                  {/* <Typography */}
-                  {/* component="li" */}
-                  {/* variant="subtitle1" */}
-                  {/* align="center" */}
-                  {/* key={line} */}
-                  {/* > */}
-                  {/* {line} */}
-                  {/* </Typography> */}
-                  {/* ))} */}
-                </ul>
-              </CardContent>
-              <CardActions
-                sx={{
-                  direction: "row",
-                  justifyContent: "center",
-                  alignItems: "center",
-                }}
-              >
-                <Button
-                  sx={{
-                    width: {
-                      xs: "100%", //0
-                      sm: "50%", //600
-                      md: "50%", //900
-                      lg: "50%", //1200
-                      xl: "50%", //1536
-                    },
-                  }}
-                  variant={"contained"}
-                  onClick={()=>{
-                    onId(tier._id);
-                    navigte("/project");
-
-                  }}
-                >
-                  {"כניסה לפרויקט"}
-                </Button>
-              </CardActions>
-            </Card>
-          </Grid>
-        ))}
-      </Grid>
+                  <Button
+                    sx={{
+                      width: {
+                        xs: "100%", //0
+                        sm: "50%", //600
+                        md: "50%", //900
+                        lg: "50%", //1200
+                        xl: "50%", //1536
+                      },
+                    }}
+                    variant={"contained"}
+                    onClick={() => {
+                      onId(tier._id);
+                      navigte("/project");
+                    }}
+                  >
+                    {"כניסה לפרויקט"}
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      </Box>
     </>
   );
 };
